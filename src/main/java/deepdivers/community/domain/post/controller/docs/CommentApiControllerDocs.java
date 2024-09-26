af49915de48a6f3a9004b50a1cf88be834f4a492
jihwan <jihwan1535@nate.com>
2024-09-26 20:14:16 +0900

    docs(#42): 댓글 작성 swagger 작성

--- src/main/java/deepdivers/community/domain/post/controller/docs/CommentApiControllerDocs.java
+++ src/main/java/deepdivers/community/domain/post/controller/docs/CommentApiControllerDocs.java
@@ -16,24 +16,18 @@ public interface CommentApiControllerDocs {
 
     @Operation(summary = "댓글 작성", description = "게시글 댓글을 작성하는 기능")
     @ApiResponse(
-            responseCode = "200",
+            responseCode = "1300",
             description = """
-                    성공
+                    1. 댓글 작성에 성공하였습니다.
                     """
     )
     @ApiResponse(
-            responseCode = "400",
-            description = """
-                    1. Request의 변수 정보가 ...
-                    """,
-            content = @Content(schema = @Schema(implementation = ExceptionResponse.class))
-    )
-    @ApiResponse(
-            responseCode = "404",
-            description = """
-                    1. 존재하지 않는 ...
-                    """,
-            content = @Content(schema = @Schema(implementation = ExceptionResponse.class))
+        responseCode = "2202, \n9000~9005",
+        description = """
+            1. 해당 게시글을 찾을 수 없습니다.
+            2. 토큰 관련 예외입니다.
+            """,
+        content = @Content(schema = @Schema(implementation = ExceptionResponse.class))
     )
     ResponseEntity<NoContent> writeCommentOnPosts(Member member, WriteCommentRequest request);
 
