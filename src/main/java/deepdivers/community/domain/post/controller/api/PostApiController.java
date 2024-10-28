947b8e3ffc6f18d75fd1880a298915cac9590da2
아윤봄 <119410207+AYunBOM@users.noreply.github.com>
2024-10-28 22:18:11 +0900

    feat(#80): 게시글 좋아요 관련 API 엔드포인트 추가
    # Conflicts:
    #	src/main/java/deepdivers/community/domain/post/controller/api/PostApiController.java

--- src/main/java/deepdivers/community/domain/post/controller/api/PostApiController.java
+++ src/main/java/deepdivers/community/domain/post/controller/api/PostApiController.java
@@ -7,6 +7,8 @@ import deepdivers.community.domain.post.controller.docs.PostApiControllerDocs;
 import deepdivers.community.domain.post.dto.request.PostSaveRequest;
 import deepdivers.community.domain.post.dto.response.PostImageUploadResponse;
 import deepdivers.community.domain.post.dto.response.PostSaveResponse;
+import deepdivers.community.domain.post.dto.request.LikeRequest;
+import deepdivers.community.domain.post.service.LikeService;
 import deepdivers.community.domain.post.service.PostService;
 import deepdivers.community.global.security.jwt.Auth;
 import jakarta.validation.Valid;
@@ -22,12 +24,13 @@ import org.springframework.web.multipart.MultipartFile;
 public class PostApiController implements PostApiControllerDocs {
 
 	private final PostService postService;
+	private final LikeService likeService;
 
 	@Override
 	@PostMapping("/upload")
 	public ResponseEntity<API<PostSaveResponse>> createPost(
-		@Auth final Member member,
-		@Valid @RequestBody final PostSaveRequest request
+			@Auth final Member member,
+			@Valid @RequestBody final PostSaveRequest request
 	) {
 		final API<PostSaveResponse> response = postService.createPost(request, member);
 		return ResponseEntity.ok(response);
@@ -36,9 +39,9 @@ public class PostApiController implements PostApiControllerDocs {
 	@Override
 	@PostMapping("/update/{postId}")
 	public ResponseEntity<API<PostSaveResponse>> updatePost(
-		@Auth final Member member,
-		@PathVariable final Long postId,
-		@Valid @RequestBody final PostSaveRequest request
+			@Auth final Member member,
+			@PathVariable final Long postId,
+			@Valid @RequestBody final PostSaveRequest request
 	) {
 		final API<PostSaveResponse> response = postService.updatePost(postId, request, member);
 		return ResponseEntity.ok(response);
@@ -47,8 +50,8 @@ public class PostApiController implements PostApiControllerDocs {
 	@Override
 	@PatchMapping("/delete/{postId}")
 	public ResponseEntity<NoContent> deletePost(
-		@Auth final Member member,
-		@PathVariable final Long postId
+			@Auth final Member member,
+			@PathVariable final Long postId
 	) {
 		final NoContent response = postService.deletePost(postId, member);
 		return ResponseEntity.ok(response);
@@ -56,10 +59,29 @@ public class PostApiController implements PostApiControllerDocs {
 
 	@PostMapping(value = "/upload/image", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
 	public ResponseEntity<API<PostImageUploadResponse>> uploadPostImage(
-		@RequestParam final MultipartFile imageFile
+			@RequestParam final MultipartFile imageFile
 	) {
 		API<PostImageUploadResponse> response = postService.uploadPostImage(imageFile);
 		return ResponseEntity.ok(response);
 	}
+
+	@PostMapping("/like")
+	public ResponseEntity<NoContent> likePost(
+			@Auth final Member member,
+			@RequestBody final LikeRequest request
+	) {
+		final NoContent response = likeService.likePost(request, member.getId());
+		return ResponseEntity.ok(response);
+	}
+
+	@PostMapping("/unlike")
+	public ResponseEntity<NoContent> unlikePost(
+			@Auth final Member member,
+			@RequestBody final LikeRequest request
+	) {
+		final NoContent response = likeService.unlikePost(request, member.getId());
+		return ResponseEntity.ok(response);
+	}
+
 }
 
