62592468d0aeb0a570b2b937c697bdbf4b52ce90
아윤봄 <119410207+AYunBOM@users.noreply.github.com>
2024-10-28 22:51:21 +0900

    fix(#80): PostRepository.java 구문 오류 수정

--- src/main/java/deepdivers/community/domain/post/repository/PostRepository.java
+++ src/main/java/deepdivers/community/domain/post/repository/PostRepository.java
@@ -26,5 +26,5 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 	@Modifying
 	@Query("UPDATE Post p SET p.likeCount = p.likeCount - 1 WHERE p.id = :postId")
 	void decrementLikeCount(Long postId);
-}
+
 }
