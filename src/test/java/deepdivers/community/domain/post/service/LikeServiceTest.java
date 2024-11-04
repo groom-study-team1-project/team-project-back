f93966ec99dcc1c0d3544591463c6a6bfcc65e7f
아윤봄 <119410207+AYunBOM@users.noreply.github.com>
2024-11-04 19:44:44 +0900

    feat(#80): 게시글 좋아요/좋아요 취소 Test

--- src/test/java/deepdivers/community/domain/post/service/LikeServiceTest.java
+++ src/test/java/deepdivers/community/domain/post/service/LikeServiceTest.java
@@ -4,7 +4,10 @@ import deepdivers.community.domain.member.repository.MemberRepository;
 import deepdivers.community.domain.post.dto.request.LikeRequest;
 import deepdivers.community.domain.post.model.PostContent;
 import deepdivers.community.domain.post.model.PostTitle;
+import deepdivers.community.domain.post.model.like.Like;
+import deepdivers.community.domain.post.model.like.LikeId;
 import deepdivers.community.domain.post.model.vo.CategoryStatus;
+import deepdivers.community.domain.post.model.vo.LikeTarget;
 import deepdivers.community.domain.post.repository.CategoryRepository;
 import deepdivers.community.domain.post.repository.LikeRepository;
 import deepdivers.community.domain.post.repository.PostRepository;
@@ -13,6 +16,8 @@ import deepdivers.community.domain.post.model.PostCategory;
 import deepdivers.community.domain.member.model.Member;
 import deepdivers.community.domain.post.model.vo.PostStatus;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +26,7 @@ import deepdivers.community.global.utility.encryptor.Encryptor;
 import org.mockito.Mockito;
 
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional
@@ -82,4 +88,34 @@ class LikeServiceTest {
         likeRequest = new LikeRequest(postId);
     }
 
+    @Test
+    @DisplayName("게시물 좋아요 성공 테스트")
+    void likePost_Success() {
+        // When: 좋아요 요청 수행
+        likeService.likePost(likeRequest, memberId);
+
+        // Then: 해당 게시물에 좋아요가 되었는지 확인
+        LikeId likeId = LikeId.of(postId, memberId, LikeTarget.POST);
+        Like savedLike = likeRepository.findById(likeId).orElseThrow();
+
+        assertEquals(likeId, savedLike.getId());
+    }
+
+    @Test
+    @DisplayName("게시물 좋아요 취소 성공 테스트")
+    void unlikePost_Success() {
+        // Given: 이미 좋아요가 된 상태 설정
+        likeService.likePost(likeRequest, memberId);
+
+        // When: 좋아요 취소 요청 수행
+        likeService.unlikePost(likeRequest, memberId);
+
+        // Then: 좋아요가 취소 되었는지 확인
+        LikeId likeId = LikeId.of(postId, memberId, LikeTarget.POST);
+        boolean likeExists = likeRepository.existsById(likeId);
+
+        assertEquals(false, likeExists);
+    }
 }
+
+
