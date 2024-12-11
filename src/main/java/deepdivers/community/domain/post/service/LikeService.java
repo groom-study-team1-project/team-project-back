3774871387c52c9227faf4cbef20207866ff2e53
Seio924 <seio9241@gmail.com>
2024-12-11 18:45:41 +0900

    feat(#100): 게시글 좋아요 관련 로직 추가

--- src/main/java/deepdivers/community/domain/post/service/LikeService.java
+++ src/main/java/deepdivers/community/domain/post/service/LikeService.java
@@ -3,8 +3,10 @@ package deepdivers.community.domain.post.service;
 import deepdivers.community.domain.common.NoContent;
 import deepdivers.community.domain.post.dto.request.LikeRequest;
 import deepdivers.community.domain.post.dto.response.statustype.CommentStatusType;
+import deepdivers.community.domain.post.dto.response.statustype.PostStatusType;
 import deepdivers.community.domain.post.exception.LikeExceptionType;
 import deepdivers.community.domain.post.model.like.Like;
+import deepdivers.community.domain.post.model.like.LikeId;
 import deepdivers.community.domain.post.model.vo.LikeTarget;
 import deepdivers.community.domain.post.repository.CommentRepository;
 import deepdivers.community.domain.post.repository.LikeRepository;
@@ -25,8 +27,7 @@ public class LikeService {
 
     public NoContent likeComment(final LikeRequest request, final Long memberId)  {
         final Like like = Like.of(request.targetId(), memberId, LikeTarget.COMMENT);
-        likeRepository.findById(like.getId())
-            .ifPresent(liked -> {throw new BadRequestException(LikeExceptionType.INVALID_ACCESS);});
+        validateExistsLike(like.getId());
 
         likeRepository.save(like);
         commentRepository.incrementLikeCount(request.targetId());
@@ -36,14 +37,44 @@ public class LikeService {
 
     public NoContent unlikeComment(final LikeRequest request, final Long memberId)  {
         final Like like = Like.of(request.targetId(), memberId, LikeTarget.COMMENT);
-        if (!likeRepository.existsById(like.getId())) {
-            throw new BadRequestException(LikeExceptionType.INVALID_ACCESS);
-        }
+        validateNotExistsLike(like.getId());
 
         likeRepository.delete(like);
         commentRepository.decrementLikeCount(request.targetId());
 
         return NoContent.from(CommentStatusType.COMMENT_UNLIKE_SUCCESS);
+    }
+
+    public NoContent likePost(final LikeRequest request, final Long memberId)  {
+        final Like like = Like.of(request.targetId(), memberId, LikeTarget.POST);
+        validateExistsLike(like.getId());
+
+        likeRepository.save(like);
+        postRepository.incrementLikeCount(request.targetId());
+
+        return NoContent.from(PostStatusType.POST_LIKE_SUCCESS);
+    }
+
+    public NoContent unlikePost(final LikeRequest request, final Long memberId)  {
+        final Like like = Like.of(request.targetId(), memberId, LikeTarget.POST);
+        validateNotExistsLike(like.getId());
+
+        likeRepository.delete(like);
+        postRepository.decrementLikeCount(request.targetId());
+
+        return NoContent.from(PostStatusType.POST_UNLIKE_SUCCESS);
+    }
+
+    private void validateExistsLike(final LikeId likeId) {
+        if (likeRepository.existsById(likeId)) {
+            throw new BadRequestException(LikeExceptionType.INVALID_ACCESS);
+        }
+    }
+
+    private void validateNotExistsLike(final LikeId likeId) {
+        if (!likeRepository.existsById(likeId)) {
+            throw new BadRequestException(LikeExceptionType.INVALID_ACCESS);
+        }
     }
 
 }
